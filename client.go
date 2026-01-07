e4bebe8c11e05f08bec91cb59203d2db5e2e81b6
kodeart <kodeart@users.noreply.github.com>
2026-01-07 18:15:03 +0100

    feat: added more methods, grpc client constructor made easier

--- client.go
+++ client.go
@@ -13,48 +13,46 @@ import (
 type Client struct {
     grpcClient pb.IdentityServiceClient
     conn       *grpc.ClientConn
-    tenantSlug string
 }
 
-func NewClient(addr, tenantSlug string) (*Client, error) {
+func NewClient(addr string) (*Client, error) {
     conn, err := grpc.NewClient(addr,
         grpc.WithTransportCredentials(insecure.NewCredentials()),
     )
     if err != nil {
-        return nil, fmt.Errorf("could not connect to identity platform: %w", err)
+        return nil, err //fmt.Errorf("could not connect to identity platform: %w", err)
     }
     return &Client{
         grpcClient: pb.NewIdentityServiceClient(conn),
         conn:       conn,
-        tenantSlug: tenantSlug,
     }, nil
 }
 
 // AuthenticateWithProvider is what the frontend calls
 // after getting a token from the external auth provider.
-func (c *Client) AuthenticateWithProvider(ctx context.Context, providerToken string) (*pb.AuthenticateResponse, error) {
+func (c *Client) AuthenticateWithProvider(ctx context.Context, tenantSlug, providerToken string) (*pb.AuthenticateResponse, error) {
     return c.grpcClient.Authenticate(ctx, &pb.AuthenticateRequest{
+        TenantSlug: tenantSlug,
         Credentials: &pb.AuthenticateRequest_ProviderToken{
             ProviderToken: providerToken,
         },
-        TenantSlug: c.tenantSlug,
     })
 }
 
-func (c *Client) AuthenticateWithCredentials(ctx context.Context, email, password string) (*pb.AuthenticateResponse, error) {
+func (c *Client) AuthenticateWithCredentials(ctx context.Context, tenantSlug, email, password string) (*pb.AuthenticateResponse, error) {
     return c.grpcClient.Authenticate(ctx, &pb.AuthenticateRequest{
+        TenantSlug: tenantSlug,
         Credentials: &pb.AuthenticateRequest_Credential{
             Credential: &pb.UserCredentials{
                 Email:    email,
                 Password: password,
             },
         },
-        TenantSlug: c.tenantSlug,
     })
 }
 
-// ValidateSession is used by the middleware to check
-// if the JWT from the request is valid.
+// ValidateSession is used by the middleware
+// to check if the JWT from the request is valid.
 func (c *Client) ValidateSession(ctx context.Context, token string) (*pb.User, error) {
     resp, err := c.grpcClient.ValidateSession(ctx, &pb.ValidateSessionRequest{Token: token})
     if err != nil {
