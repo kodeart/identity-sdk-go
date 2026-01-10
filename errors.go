aa8ad888f0bb8047f0c5639d7e180eff956aaf88
kodeart <kodeart@users.noreply.github.com>
2026-01-10 18:03:32 +0100

    refactor: simplification and direct json unmarshalling

--- errors.go
+++ errors.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/kodeart/go-problem/v2"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
@@ -11,23 +12,11 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// ProviderError represents an error structure returned by an
-// identity provider, which may include generic or detailed errors.
-type ProviderError struct {
-	// Generic style (i.e. Firebase, etc)
-	Error string `json:"error"`
-	// Clerk format
-	Errors []struct {
-		Code    string `json:"code"`
-		Message string `json:"message"`
-	} `json:"errors"`
-}
-
 // AsProblem converts a gRPC error to a problem.Problem
 // and attaches request-specific information if any.
 func AsProblem(r *http.Request, err error) *problem.Problem {
 	st := status.Convert(err)
-	msg := st.Message()
+	rawMsg := st.Message()
 	scheme := "http"
 	if r.TLS != nil {
 		scheme = "https"
@@ -35,34 +24,35 @@ func AsProblem(r *http.Request, err error) *problem.Problem {
 	p := &problem.Problem{
 		Status:   CodeToHttpStatus(st.Code()),
 		Instance: fmt.Sprintf(" %s://%s%s", scheme, r.Host, r.RequestURI),
-		Detail:   msg,
+		Detail:   rawMsg,
 		Title:    st.Code().String(),
+		Type:     getType(st.Code(), scheme, r.Host),
 	}
-	errs := make(map[string]any)
-	if json.Valid([]byte(msg)) {
-		var pErr ProviderError
-		if jsonErr := json.Unmarshal([]byte(msg), &pErr); jsonErr == nil {
-			p.Detail = "The identity provicer returned as error"
-			for _, e := range pErr.Errors {
-				errs[e.Code] = e.Message
+	if idx := strings.Index(rawMsg, "{"); idx != -1 {
+		jsonPart := rawMsg[idx:]
+		var rawData map[string]any
+		if jsonErr := json.Unmarshal([]byte(jsonPart), &rawData); jsonErr == nil {
+			p.WithExtension("errors", rawData)
+			p.Detail = strings.Trim(rawMsg[:idx], ": ")
+			if p.Detail == "" {
+				p.Detail = "The identity provider returned as error"
 			}
 		}
 	}
 	// Extract SDK details (BadRequest / FieldViolations)
 	for _, detail := range st.Details() {
 		if t, ok := detail.(*errdetails.BadRequest); ok {
+			p.Type = getType(codes.InvalidArgument, scheme, r.Host)
 			for _, v := range t.GetFieldViolations() {
-				errs[v.GetField()] = v.GetDescription()
+				p.WithExtension(v.GetField(), v.GetDescription())
 			}
 		}
-	}
-	if len(errs) > 0 {
-		p.WithExtension("errors", errs)
 	}
 	return p
 }
 
-// CodeToHttpStatus converts gRPC error code to its corresponding HTTP status code.
+// CodeToHttpStatus converts gRPC error code
+// to its corresponding HTTP status code.
 func CodeToHttpStatus(code codes.Code) int {
 	switch code {
 	case codes.OK:
@@ -88,4 +78,25 @@ func CodeToHttpStatus(code codes.Code) int {
 	default:
 		return http.StatusInternalServerError
 	}
+}
+
+// getType returns a URI indicating the type of
+// error based on the provided gRPC error code.
+func getType(code codes.Code, scheme string, host string) string {
+	var t string
+	switch code {
+	case codes.Unauthenticated:
+		t = "invalid-session"
+	case codes.InvalidArgument:
+		t = "validation-failed"
+	case codes.DeadlineExceeded:
+		t = "gateway-timeout"
+	case codes.Unavailable:
+		t = "service-unavailable"
+	case codes.FailedPrecondition:
+		t = "service-error"
+	default:
+		t = "internal-error" // Unknown
+	}
+	return fmt.Sprintf("%s://%s/errors/%s", scheme, host, t)
 }
